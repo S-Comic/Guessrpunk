f27eefc1a6887f74f56289afb74ab2eaeffc18ba
Samuel Jule <32823294+S-Comic@users.noreply.github.com>
2023-02-23 22:14:43 +0000

    More images and image name standardisation

--- components/imageArray.js
+++ components/imageArray.js
@@ -1,53 +1,92 @@
 const imageArray = [
     {
         "id" : 0,
-        "url" : "/stitch.jpg",
+        "url" : "/0.jpg",
         "location" : [0.1191, 0.0623],
         "difficulty" : "easy"
     },
     {
         "id" : 1,
-        "url" : "/stitch6.jpg",
+        "url" : "/1.jpg",
         "location" : [(0.2891 - 0.1781), 0.0892],
         "difficulty" : "easy"
     },
     {
         "id" : 2,
-        "url" : "/stitch3.jpg",
+        "url" : "/2.jpg",
         "location" : [(0.2891 - 0.2206), 0.1255],
         "difficulty" : "easy"
     },
     {
         "id" : 3,
-        "url" : "/stitch4.jpg",
+        "url" : "/3.jpg",
         "location" : [(0.2891 - 0.1474), 0.1019],
         "difficulty" : "easy"
     },
 
     {
         "id" : 4,
-        "url" : "/stitch5.jpg",
+        "url" : "/4.jpg",
         "location" : [(0.2891 - 0.0889), 0.0902],
         "difficulty" : "easy"
     },
     {
         "id" : 5,
-        "url" : "/stitch7.jpg",
+        "url" : "/5.jpg",
         "location" : [(0.2891 - 0.1238), 0.1238],
         "difficulty" : "easy"
     },
     {
         "id" : 6,
-        "url" : "/stitch8.jpg",
+        "url" : "/6.jpg",
         "location" : [(0.2891 - 0.1266), 0.1177],
         "difficulty" : "easy"
     },
     {
         "id" : 7,
-        "url" : "/stitch9.jpg",
+        "url" : "/7.jpg",
         "location" : [(0.2891 - 0.2383), 0.0526],
         "difficulty" : "easy"
     },
+    {
+        "id" : 8,
+        "url" : "/8.jpg",
+        "location" : [(0.2891 - 0.2054), 0.1052],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 9,
+        "url" : "/9.jpg",
+        "location" : [(0.2891 - 0.1659), 0.1309],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 10,
+        "url" : "/10.jpg",
+        "location" : [(0.2891 - 0.1427), 0.1217],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 11,
+        "url" : "/11.jpg",
+        "location" : [(0.2891 - 0.1480), 0.1135],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 12,
+        "url" : "/12.jpg",
+        "location" : [(0.2891 - 0.1629), 0.0688],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 13,
+        "url" : "/13.jpg",
+        "location" : [(0.2891 - 0.2001), 0.0450],
+        "difficulty" : "easy"
+    },
+    
+    
+    
     
 ]
 
