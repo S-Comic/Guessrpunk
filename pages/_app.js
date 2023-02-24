60cf2f7caff7f39a348836a76d4edcb0e1e73c52
Samuel Jule <32823294+S-Comic@users.noreply.github.com>
2023-02-24 18:51:06 +0000

    Added Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '@/styles/globals.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
+import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+
 
 
 export default function App({ Component, pageProps }) {
   
-  return <Component {...pageProps} />
+  return(
+    <>
+    <Component {...pageProps} />
+    <Analytics />
+    </>
+  ) 
 }
