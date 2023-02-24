cf4220cc53f16ac167c2da915a604e53c5636710
Samuel Jule <32823294+S-Comic@users.noreply.github.com>
2023-02-24 17:24:49 +0000

    Fixed imageArray

--- components/imageArray.js
+++ components/imageArray.js
@@ -1,155 +1,155 @@
 const imageArray = [
-    // {
-    //     "id" : 0,
-    //     "url" : "/0.jpg",
-    //     "location" : [0.1191, 0.0623],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 1,
-    //     "url" : "/1.jpg",
-    //     "location" : [(0.2891 - 0.1781), 0.0892],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 2,
-    //     "url" : "/2.jpg",
-    //     "location" : [(0.2891 - 0.2206), 0.1255],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 3,
-    //     "url" : "/3.jpg",
-    //     "location" : [(0.2891 - 0.1474), 0.1019],
-    //     "difficulty" : "easy"
-    // },
+    {
+        "id" : 0,
+        "url" : "/0.jpg",
+        "location" : [0.1191, 0.0623],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 1,
+        "url" : "/1.jpg",
+        "location" : [(0.2891 - 0.1781), 0.0892],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 2,
+        "url" : "/2.jpg",
+        "location" : [(0.2891 - 0.2206), 0.1255],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 3,
+        "url" : "/3.jpg",
+        "location" : [(0.2891 - 0.1474), 0.1019],
+        "difficulty" : "easy"
+    },
 
-    // {
-    //     "id" : 4,
-    //     "url" : "/4.jpg",
-    //     "location" : [(0.2891 - 0.0889), 0.0902],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 5,
-    //     "url" : "/5.jpg",
-    //     "location" : [(0.2891 - 0.1238), 0.1238],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 6,
-    //     "url" : "/6.jpg",
-    //     "location" : [(0.2891 - 0.1266), 0.1177],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 7,
-    //     "url" : "/7.jpg",
-    //     "location" : [(0.2891 - 0.2383), 0.0526],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 8,
-    //     "url" : "/8.jpg",
-    //     "location" : [(0.2891 - 0.2054), 0.1052],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 9,
-    //     "url" : "/9.jpg",
-    //     "location" : [(0.2891 - 0.1659), 0.1309],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 10,
-    //     "url" : "/10.jpg",
-    //     "location" : [(0.2891 - 0.1427), 0.1217],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 11,
-    //     "url" : "/11.jpg",
-    //     "location" : [(0.2891 - 0.1480), 0.1135],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 12,
-    //     "url" : "/12.jpg",
-    //     "location" : [(0.2891 - 0.1629), 0.0688],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 13,
-    //     "url" : "/13.jpg",
-    //     "location" : [(0.2891 - 0.2001), 0.0450],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 14,
-    //     "url" : "/14.jpg",
-    //     "location" : [(0.2891 - 0.1093), 0.1473],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 15,
-    //     "url" : "/15.jpg",
-    //     "location" : [(0.2891 - 0.2180), 0.1865],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 16,
-    //     "url" : "/16.jpg",
-    //     "location" : [(0.2891 - 0.0873), 0.0451],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 17,
-    //     "url" : "/17.jpg",
-    //     "location" : [(0.2891 - 0.1558), 0.0784],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 18,
-    //     "url" : "/18.jpg",
-    //     "location" : [(0.2891 - 0.2171), 0.1546],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 19,
-    //     "url" : "/19.jpg",
-    //     "location" : [(0.2891 - 0.2606), 0.1859],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 20,
-    //     "url" : "/20.jpg",
-    //     "location" : [(0.2891 - 0.2042), 0.0358],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 21,
-    //     "url" : "/21.jpg",
-    //     "location" : [(0.2891 - 0.1394), 0.0420],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 22,
-    //     "url" : "/22.jpg",
-    //     "location" : [(0.2891 - 0.0820), 0.1241],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 23,
-    //     "url" : "/23.jpg",
-    //     "location" : [(0.2891 - 0.0906), 0.1338],
-    //     "difficulty" : "easy"
-    // },
-    // {
-    //     "id" : 24,
-    //     "url" : "/24.jpg",
-    //     "location" : [(0.2891 - 0.2063), 0.1338],
-    //     "difficulty" : "easy"
-    // },
+    {
+        "id" : 4,
+        "url" : "/4.jpg",
+        "location" : [(0.2891 - 0.0889), 0.0902],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 5,
+        "url" : "/5.jpg",
+        "location" : [(0.2891 - 0.1238), 0.1238],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 6,
+        "url" : "/6.jpg",
+        "location" : [(0.2891 - 0.1266), 0.1177],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 7,
+        "url" : "/7.jpg",
+        "location" : [(0.2891 - 0.2383), 0.0526],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 8,
+        "url" : "/8.jpg",
+        "location" : [(0.2891 - 0.2054), 0.1052],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 9,
+        "url" : "/9.jpg",
+        "location" : [(0.2891 - 0.1659), 0.1309],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 10,
+        "url" : "/10.jpg",
+        "location" : [(0.2891 - 0.1427), 0.1217],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 11,
+        "url" : "/11.jpg",
+        "location" : [(0.2891 - 0.1480), 0.1135],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 12,
+        "url" : "/12.jpg",
+        "location" : [(0.2891 - 0.1629), 0.0688],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 13,
+        "url" : "/13.jpg",
+        "location" : [(0.2891 - 0.2001), 0.0450],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 14,
+        "url" : "/14.jpg",
+        "location" : [(0.2891 - 0.1093), 0.1473],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 15,
+        "url" : "/15.jpg",
+        "location" : [(0.2891 - 0.2180), 0.1865],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 16,
+        "url" : "/16.jpg",
+        "location" : [(0.2891 - 0.0873), 0.0451],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 17,
+        "url" : "/17.jpg",
+        "location" : [(0.2891 - 0.1558), 0.0784],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 18,
+        "url" : "/18.jpg",
+        "location" : [(0.2891 - 0.2171), 0.1546],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 19,
+        "url" : "/19.jpg",
+        "location" : [(0.2891 - 0.2606), 0.1859],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 20,
+        "url" : "/20.jpg",
+        "location" : [(0.2891 - 0.2042), 0.0358],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 21,
+        "url" : "/21.jpg",
+        "location" : [(0.2891 - 0.1394), 0.0420],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 22,
+        "url" : "/22.jpg",
+        "location" : [(0.2891 - 0.0820), 0.1241],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 23,
+        "url" : "/23.jpg",
+        "location" : [(0.2891 - 0.0906), 0.1338],
+        "difficulty" : "easy"
+    },
+    {
+        "id" : 24,
+        "url" : "/24.jpg",
+        "location" : [(0.2891 - 0.2063), 0.1338],
+        "difficulty" : "easy"
+    },
     {
         "id" : 25,
         "url" : "/25.jpg",
